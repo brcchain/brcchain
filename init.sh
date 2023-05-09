2de89c01c173e7652f3c1380f264d5d511003a73
timi202 <116780733+timi202@users.noreply.github.com>
2023-05-09 15:13:53 +0800

    init proj

---
+++ init.sh
@@ -0,0 +1,129 @@
+KEY="alice"
+CHAINID="brc_12123-1"
+MONIKER="local"
+KEYRING="test"
+LOGLEVEL="info"
+HOMEDIR="$HOME/.brc"
+# to trace evm
+TRACE="--trace"
+
+# validate dependencies are installed
+#command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
+
+# Reinstall daemon
+rm -rf $HOMEDIR/data/
+rm -rf $HOMEDIR/config/
+
+# Set client config
+ethermintd config keyring-backend $KEYRING --home $HOMEDIR
+ethermintd config chain-id $CHAINID --home $HOMEDIR
+
+# if $KEY exists it should be deleted
+ethermintd keys add $KEY --keyring-backend $KEYRING --home $HOMEDIR
+
+# Set moniker and chain-id for cosmos (Moniker can be anything, chain-id must be an integer)
+ethermintd init $MONIKER --chain-id $CHAINID --home $HOMEDIR
+
+cp $HOMEDIR/config/genesis.json $HOMEDIR/config/tmp_genesis.json
+
+# Change parameter abrc denominations to abrc
+cat $HOMEDIR/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["slashing"]["params"]["signed_blocks_window"]="5000"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["amount"]="64"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["max_deposit_period"]="259200s"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["gov"]["voting_params"]["voting_period"]="259200s"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+cat $HOMEDIR/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+#cat $HOMEDIR/config/genesis.json | jq '.app_state["evm"]["accounts"][0]["address"]="0x4200000000000000000000000000000000000042"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq -r --arg CODE "$CODE" '.app_state["evm"]["accounts"][0]["code"]=$CODE' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+
+# Set gas limit in genesis
+cat $HOMEDIR/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="40000000"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# Set claims start time
+node_address=$(ethermintd keys list --home $HOMEDIR | grep  "address: " | cut -c12-)
+current_date=$(date -u +"%Y-%m-%dT%TZ")
+cat $HOMEDIR/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# Set claims records for validator account
+amount_to_claim=10000
+cat $HOMEDIR/config/genesis.json | jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# Set claims decay
+cat $HOMEDIR/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["duration_of_decay"]="1000000s"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["duration_until_decay"]="100000s"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# Set oracle params
+#cat $HOMEDIR/config/genesis.json | jq -r --arg node_address "$node_address" '.app_state["oracle"]["params"]["admin"]=$node_address' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq  '.app_state["oracle"]["params"]["scriptId"]=111' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq  '.app_state["oracle"]["params"]["multiplier"]=1000000' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq  '.app_state["oracle"]["coinSymbolList"][0]["id"]=0' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq  '.app_state["oracle"]["coinSymbolList"][0]["symbol"]="BTC"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+#cat $HOMEDIR/config/genesis.json | jq  '.app_state["oracle"]["coinSymbolList"][0]["isDeleted"]=false' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# disable produce empty block
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/127.0.0.1:26657/0.0.0.0:26657/g' $HOMEDIR/config/config.toml
+    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOMEDIR/config/config.toml
+    sed -i '' '$H;x;1,/enable = false/s/enable = false/enable = true/;1d' $HOMEDIR/config/app.toml
+    sed -i '' ' s/swagger = false/swagger = true/g' $HOMEDIR/config/app.toml
+    sed -i '' 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' $HOMEDIR/config/app.toml
+  else
+    sed -i 's/127.0.0.1:26657/0.0.0.0:26657/g' $HOMEDIR/config/config.toml
+    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOMEDIR/config/config.toml
+    sed -i '$H;x;1,/enable = false/s/enable = false/enable = true/;1d' $HOMEDIR/config/app.toml
+    sed -i ' s/swagger = false/swagger = true/g' $HOMEDIR/config/app.toml
+    sed -i 's/enabled-unsafe-cors = false/enabled-unsafe-cors = true/g' $HOMEDIR/config/app.toml
+fi
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "2s"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "1s"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "1s"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "2s"/g' $HOMEDIR/config/config.toml
+      sed -i '' 's/timeout_broadcast_tx_commit = "2m30s"/timeout_broadcast_tx_commit = "10s"/g' $HOMEDIR/config/config.toml
+  else
+      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_propose = "3s"/timeout_propose = "2s"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "1s"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "1s"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "500ms"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_commit = "5s"/timeout_commit = "2s"/g' $HOMEDIR/config/config.toml
+      sed -i 's/timeout_broadcast_tx_commit = "2m30s"/timeout_broadcast_tx_commit = "10s"/g' $HOMEDIR/config/config.toml
+fi
+
+# Allocate genesis accounts (cosmos formatted addresses)
+ethermintd add-genesis-account $KEY 1000000000000000000000000000abrc --keyring-backend $KEYRING --home $HOMEDIR
+
+# Update total supply with claim values
+validators_supply=$(cat $HOMEDIR/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["amount"]')
+## brc is required to add this big numbers
+## total_supply=$(brc <<< "$amount_to_claim+$validators_supply")
+total_supply=1000000000000000000000000000
+cat $HOMEDIR/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["denom"]="abrc"' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+cat $HOMEDIR/config/genesis.json | jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' > $HOMEDIR/config/tmp_genesis.json && mv $HOMEDIR/config/tmp_genesis.json $HOMEDIR/config/genesis.json
+
+# Sign genesis transaction
+ethermintd gentx $KEY 1000brc --keyring-backend $KEYRING --chain-id $CHAINID --home $HOMEDIR
+
+# Collect genesis tx
+ethermintd collect-gentxs --home $HOMEDIR
+
+# Run this to ensure everything worked and that the genesis file is setup correctly
+ethermintd validate-genesis --home $HOMEDIR
+
+#Start the node
+echo ethermintd start --pruning=nothing $TRACE --log_level $LOGLEVEL --json-rpc.api eth,txpool,net,web3 --home $HOMEDIR
---
+++ types/coin.go
@@ -0,0 +1,63 @@
+// Copyright 2021 Evmos Foundation
+// This file is part of Evmos' Ethermint library.
+//
+// The Ethermint library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Ethermint library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Ethermint library. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
+package types
+
+import (
+	"math/big"
+
+	sdkmath "cosmossdk.io/math"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	// AttoBrc defines the default coin denomination used in Ethermint in:
+	//
+	// - Staking parameters: denomination used as stake in the dPoS chain
+	// - Mint parameters: denomination minted due to fee distribution rewards
+	// - Governance parameters: denomination used for spam prevention in proposal deposits
+	// - Crisis parameters: constant fee denomination used for spam prevention to check broken invariant
+	// - EVM parameters: denomination used for running EVM state transitions in Ethermint.
+	AttoBrc string = "abrc"
+
+	// BaseDenomUnit defines the base denomination unit for Brcs.
+	// 1 brc = 1x10^{BaseDenomUnit} abrc
+	BaseDenomUnit = 18
+
+	// DefaultGasPrice is default gas price for evm transactions
+	DefaultGasPrice = 20
+)
+
+// PowerReduction defines the default power reduction value for staking
+var PowerReduction = sdkmath.NewIntFromBigInt(new(big.Int).Exp(big.NewInt(10), big.NewInt(BaseDenomUnit), nil))
+
+// NewBrcCoin is a utility function that returns an "abrc" coin with the given sdkmath.Int amount.
+// The function will panic if the provided amount is negative.
+func NewBrcCoin(amount sdkmath.Int) sdk.Coin {
+	return sdk.NewCoin(AttoBrc, amount)
+}
+
+// NewBrcDecCoin is a utility function that returns an "abrc" decimal coin with the given sdkmath.Int amount.
+// The function will panic if the provided amount is negative.
+func NewBrcDecCoin(amount sdkmath.Int) sdk.DecCoin {
+	return sdk.NewDecCoin(AttoBrc, amount)
+}
+
+// NewBrcCoinInt64 is a utility function that returns an "abrc" coin with the given int64 amount.
+// The function will panic if the provided amount is negative.
+func NewBrcCoinInt64(amount int64) sdk.Coin {
+	return sdk.NewInt64Coin(AttoBrc, amount)
+}
